c4e41107af9b8acd86e747ea833ee0001d960071
Clint Losee <clint.losee@gmail.com>
2021-06-30 13:53:54 -0600

    Cleanup view.go

--- views/view.go
+++ views/view.go
@@ -1,13 +1,19 @@
 package views
 
-import "html/template"
+import (
+	"html/template"
+	"path/filepath"
+)
+
+var (
+	LayoutDir   string = "views/layouts/"
+	TemplateExt string = ".gohtml"
+)
 
 func NewView(layout string, files ...string) *View {
 	files = append(
 		files,
-		"views/layouts/bootstrap.gohtml",
-		"views/layouts/navbar.gohtml",
-		"views/layouts/footer.gohtml",
+		layoutFiles()...,
 	)
 
 	t, err := template.ParseFiles(files...)
@@ -24,4 +30,12 @@ func NewView(layout string, files ...string) *View {
 type View struct {
 	Template *template.Template
 	Layout   string
+}
+
+func layoutFiles() []string {
+	files, err := filepath.Glob(LayoutDir + "*" + TemplateExt)
+	if err != nil {
+		panic(err)
+	}
+	return files
 }
