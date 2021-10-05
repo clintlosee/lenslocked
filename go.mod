e5171feb1d70da80662309fa182673f3cf06c433
Clint Losee <clint.losee@gmail.com>
2021-10-05 11:36:43 -0600

    Refactored to use chi router

--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module github.com/clintlosee/lenslocked
 
 go 1.16
 
-require (
-	github.com/go-chi/chi/v5 v5.0.4 // indirect
-	github.com/gorilla/mux v1.8.0
-)
+require github.com/go-chi/chi/v5 v5.0.4
--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/clintlosee/lenslocked/views"
 
-	"github.com/gorilla/mux"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 var (
@@ -34,11 +36,22 @@ func main() {
 	contactView = views.NewView("bootstrap", "views/contact.gohtml")
 	signupView = views.NewView("bootstrap", "views/signup.gohtml")
 
-	r := mux.NewRouter()
-	r.HandleFunc("/", home)
-	r.HandleFunc("/contact", contact)
-	r.HandleFunc("/signup", signup)
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", home)
+	r.Get("/contact", contact)
+	r.Get("/signup", signup)
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Page not found", http.StatusNotFound)
+	})
+	fmt.Println("Starting the server on :7000...")
 	http.ListenAndServe(":7000", r)
+
+	// r := mux.NewRouter()
+	// r.HandleFunc("/", home)
+	// r.HandleFunc("/contact", contact)
+	// r.HandleFunc("/signup", signup)
+	// http.ListenAndServe(":7000", r)
 }
 
 func must(err error) {
